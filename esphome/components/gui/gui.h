b785deeb370930879f02271679a54fa716c2e2d3
ingbenna <samir.elbenna@gmail.com>
2024-04-04 16:34:44 +0200

    WIP

--- esphome/components/gui/gui.h
+++ esphome/components/gui/gui.h
@@ -26,6 +26,8 @@ class GuiComponent : public Component {
   static void lv_esp_log(const char *buf);
 #endif
 
+  lv_disp_drv_t disp_drv_{};
+  
   void set_display(DisplayBuffer *display) { this->display_ = display; }
   static void refresh(lv_disp_drv_t *disp_drv, const lv_area_t *area,
                           lv_color_t *buf);
@@ -37,7 +39,7 @@ class GuiComponent : public Component {
   DisplayBuffer *display_{nullptr};
 
   lv_disp_t *lv_disp_{nullptr};
-  lv_disp_drv_t disp_drv_{};
+  //lv_disp_drv_t disp_drv_{}; // Moved to top
   lv_disp_draw_buf_t draw_buf_{};
   lv_disp_rot_t get_lv_rotation();
 
